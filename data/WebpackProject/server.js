5962e405a295b83ee1533a1601fa6f75679526f8
Mark Astin <redwolf89@gmail.com>
2020-01-18 09:10:24 -0500

    end of section 9

---
+++ data/WebpackProject/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Server routes... go here
+//app.get('/hello', (req, res) => res.send({ hi: 'there'}));
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpackMiddleware = require("webpack-dev-middleware");
+  const webpack = require("webpack");
+  const webpackConfig = require("./webpack.config.js");
+  app.use(webpackMiddleware(webpack(webpackConfig)));
+}
+else {
+  app.use(express.static('dist'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html')); // specific for react router/browser history
+  })
+}
+
+app.listen(process.env.PORT || 3050, () => console.log('Listening'));
